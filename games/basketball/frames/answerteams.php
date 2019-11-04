1b1fe560f8d42b31955376c4e73049f7c34363bf
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-03 20:49:16 -0600

    Add files via upload

---
+++ games/basketball/frames/answerteams.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frame</title>
+</head>
+<body>
+	<style type="text/css">
+		body {
+			background-image: linear-gradient(to right, #F9FFC7, #F9FFE4);
+		}
+	</style>
+	<div class="search">
+		<span class="searchformspan">
+			<form autocomplete="off" id="searchform" action="answerteams.php" method="post">
+				<input style="margin-top: 100px;" placeholder="answer" autocomplete="off" id="searchid" type="text" name="game">
+			</form>
+		</span>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+<style type="text/css">
+
+	body {
+		background-color: #ffffff;
+	}
+
+	input:focus {
+		outline: none;
+	}
+
+	#searchform {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin-top: 8px;
+	}
+
+	#searchid {
+		margin-top: -5px;
+		border: solid;
+		font-size: 22px;
+		background-color: transparent;
+		border-color: transparent;
+		border-bottom-color: #000000;
+		border-width: 1px;
+		width: 400px;
+		height: 25px;
+	}
+	#menu {
+	position: absolute;
+	top: -18px;
+	left: -1px;
+	right: 0px;
+	background-color: #000000;
+	border: solid;
+	border-color: #000000;
+    clip-path: circle(50px at 15px 14px);
+	padding: 35px;
+	transition: 0.5s;
+	z-index: 50;
+}
+
+#menu:hover {
+	position: absolute;
+	top: -18px;
+	left: -1px;
+	right: 0px;
+	border: solid;
+	border-color: #ffffff;
+	border-bottom-color: #000000;
+    clip-path: circle(100%);
+	background-color: #ffffff;
+	padding: 33px;
+	transition: 0.85s;
+	z-index: 50;
+}
+.a1 {
+	padding: 2px 7px;
+	text-decoration: none;
+	color: #000000;
+	border: solid;
+	border-color: transparent;
+	background-color: transparent;
+	border-radius: 7px;
+	font-size: 25px;
+	transition: 0.5s;
+}
+
+.a1:hover {
+	padding: 2px 7px;
+	text-decoration: none;
+	color: #ffffff;
+	border: solid;
+	border-color: #000000;
+	background-color: #000000;
+	border-radius: 15px;
+	font-size: 25px;
+	transition: 0.6s;
+}
+
+#links {
+	position: absolute;
+	margin-left: 48.5%;
+	margin-right: -50%;
+	transform: translate(-50%, -50%);
+}
+
+#menulogo {
+	width: 30px;
+	height: auto;
+	position: absolute;
+	top: 16px;
+	left: 12px;
+}
+</style>
+
+
+
+
+<?php
+$result= $_POST['game'];
+$result= strtolower($result);
+echo $result;
+?>
+</body>
+</html>
---
+++ games/basketball/frames/teamsstart.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Frame</title>
+</head>
+<body>
+	<style type="text/css">
+		body {
+			background-image: linear-gradient(to right, #F9FFC7, #F9FFE4);
+		}
+	</style>
+	<div class="title">
+		<b id="toplevels"><center>Levels</center></b>
+		<span id="levels">
+			<a href="/teamsframe.php"><b id="standard">Standard</b></a>
+			<a style="margin-left: 40px;" href="/teamsframe.php"><b id="pro">Pro</b></a>
+		</span>
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+<style type="text/css">
+
+	#toplevels {
+		position: absolute;
+		top: 54px;
+		left: 50%;
+		margin-right: -50%;
+		transform: translate(-50%, -50%);
+		font-size: 55px;
+		font-family: Comic Sans MS;
+		border: solid;
+		width: 350px;
+		border-width: 2px;
+		padding-bottom: 1px;
+		border-color: transparent;
+		border-bottom-color: #000000;
+	}
+
+	a {
+		text-decoration: none;
+	}
+
+	body {
+		background-color: #ffffff;
+	}
+
+	input:focus {
+		outline: none;
+	}
+
+	#levels {
+		position: absolute;
+		top: 210px;
+		left: 50%;
+		margin-right: -50%;
+		transform: translate(-50%, -50%);
+	}
+
+	#standard {
+		font-size: 38px;
+		font-weight: 500;
+		font-family: Comic Sans MS;
+		border: solid;
+		border-color: #0EA702;
+		color: #0EA702;
+		padding-left: 5px;
+		padding-right: 5px;
+		padding-top: 0px;
+		padding-bottom: 2px;
+		border-radius: 20px;
+		background-color: transparent;
+		transition: 0.5s;
+	}
+
+	#standard:hover {
+		font-size: 38px;
+		font-weight: 500;
+		font-family: Comic Sans MS;
+		border: solid;
+		border-color: #0EA702;
+		color: #ffffff;
+		padding-left: 5px;
+		padding-right: 5px;
+		padding-top: 0px;
+		padding-bottom: 2px;
+		border-radius: 20px;
+		transform: rotate(2deg);
+		background-color: #0EA702;
+		transition: 0.5s;
+	}
+
+	#pro {
+		font-size: 38px;
+		font-weight: 500;
+		font-family: Comic Sans MS;
+		border: solid;
+		border-color: #FF6C00;
+		color: #FF6C00;
+		padding-left: 15px;
+		padding-right: 15px;
+		padding-top: 0px;
+		padding-bottom: 2px;
+		border-radius: 20px;
+		background-color: transparent;
+		transition: 0.5s;
+	}
+
+	#pro:hover {
+		
+		font-size: 38px;
+		font-weight: 500;
+		font-family: Comic Sans MS;
+		border: solid;
+		border-color: #FF6C00;
+		color: #ffffff;
+		padding-left: 15px;
+		padding-right: 15px;
+		padding-top: 0px;
+		padding-bottom: 2px;
+		border-radius: 20px;
+		transform: rotate(2deg);
+		background-color: #FF6C00;
+		transition: 0.5s;
+	}
+</style>
+
+</body>
+</html>
