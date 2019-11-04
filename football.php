b9dbb093a1d88a214841aaf5c57bad2306096c86
techmpireviews <50922628+techmpireviews@users.noreply.github.com>
2019-11-03 20:44:48 -0600

    Add files via upload

---
+++ football.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sports Games</title>
+	<link rel="icon" href="images/shortbglogo.png">
+	<link rel="stylesheet" type="text/css" href="football.css">
+	<script src="width.js"></script>
+</head>
+<header>
+	<p id="menu"><span id="links"><a class="a1" href="">Home</a>&nbsp;&nbsp;&nbsp;&nbsp; <a class="a1" href="/horses.php">Basketball Games</a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/contact.php">Football Games</a>&nbsp;&nbsp;&nbsp; <a class="a1" href="/about.php">Baseball Games</a>&nbsp;&nbsp;&nbsp;</span><img id="menulogo" src="images/menu.png"></p>
+</header>
+<body>
+	<div class="main">
+		<div class="top">
+			<div class="imgtop">
+				<img id="mainimage" src="images/grassbg.png">
+			</div>
+			<a href=""><img id="logotop" src="images/mainlogo.png"></a>
+			<!--<span id="infotop"><div class="textinfo">
+				<img id="searchimg" src="images/searchicon.png">
+				<form autocomplete="off" id="searchform" action="search.php" method="post">
+					<input autocomplete="off" id="searchid" type="text" name="search">
+					<style type="text/css">
+						#searchid {
+							position: absolute;
+							margin-top: -30px;
+							height: 17px;
+							width: 168px;
+							border-radius: 7px;
+							box-shadow: 0px;
+							text-shadow: none;
+							border: solid;
+							background-color: #ffffff;
+							border-color: #000000;
+							transition: 0.4s;
+							color: #000000;
+							border-width: 2px;
+						}
+						#searchid:hover {
+							position: absolute;
+							height: 17px;
+							margin-top: -30px;
+							width: 168px;
+							border-radius: 7px;
+							box-shadow: 0px;
+							text-shadow: none;
+							color: #F0F0F0;
+							background-color: #989696;
+							border: solid;
+							transition: 0.4s;
+							border-color: #000000;
+							border-width: 2px;
+						}
+						#submitid {
+							position: absolute;
+							background-color: #ffffff;
+							color: #000000;
+							margin-top: -10px;
+							border: solid;
+							margin-left: 177px;
+							top: 25px;
+							height: 21px;
+							transition: 0.4s;
+							border-color: #000000;
+							border-radius: 10px;
+						}
+						#submitid:hover {
+							position: absolute;
+							background-color: #000000;
+							color: #ffffff;
+							margin-left: 177px;
+							transition: 0.4s;
+							margin-top: -10px;
+							height: 21px;
+							border: solid;
+							border-color: #000000;
+							border-radius: 10px;
+						}
+						input:focus {
+							outline: none;
+						}
+        #infotop {
+	position: absolute;
+	top: 15px;
+	right: 15px;
+        clip-path: circle(21px at 265px 22px);
+	background-color: #ffffff;
+	transition: 0.5s;
+	padding: 5px 8px;
+	z-index: 15;
+}
+
+#infotop:hover {
+	position: absolute;
+	top: 15px;
+	right: 15px;
+	border-radius: 30px;
+	background-color: #ffffff;
+        clip-path: circle(100%);
+        transition: 0.5s;
+	padding: 5px 8px;
+	z-index: 15;
+}
+#searchimg {
+	margin-left: 241px;
+	width: 35px;
+	height: auto;
+	margin-top: 0px;
+}
+
+					</style>
+					<input id="submitid" type="submit" name="submit" value="search">
+				</form></div>
+			</span>-->
+		</div>
+		<div class="middle">
+			<iframe id="fr1" width="910px" height="420px" frameborder="0px" src="frames/home.php"></iframe>
+		</div>
+	</div
+	>
+</body>
+</html>
